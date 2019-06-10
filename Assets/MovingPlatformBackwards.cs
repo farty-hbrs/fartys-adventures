930d253b98a1d734dfab463439aba491c6c703b7
farty-hbrs <50452003+farty-hbrs@users.noreply.github.com>
2019-06-10 19:48:18 +0200

    Started with my level (Daniel)

---
+++ Assets/MovingPlatformBackwards.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformBackwards : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 startPos;
+    private Vector3 newPos;
+    private Vector3 lastPos;
+    private SpriteRenderer spriteRenderer;
+
+    private GameObject target = null;
+    private Vector3 offset;
+
+    void Start()
+    {
+        target = null;
+        startPos = transform.position;
+        lastPos = startPos;
+    }
+
+    void Update()
+    {
+        newPos = startPos;
+        newPos.x = newPos.x - Mathf.PingPong(Time.time * speed, 6) - 3;
+        transform.position = newPos;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        target = col.gameObject;
+        offset = target.transform.position - transform.position;
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        target = null;
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            target.transform.position = transform.position + offset;
+        }
+    }
+}
