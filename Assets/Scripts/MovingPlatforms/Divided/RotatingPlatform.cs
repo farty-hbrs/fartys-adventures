74cca7e170dd257a4865657bf450d243a0a19f60
Marco Marx <acc-git@marx.bz>
2019-06-18 21:35:51 +0200

    create scripts to define a path for platforms

---
+++ Assets/Scripts/MovingPlatforms/Divided/RotatingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingPlatform : MonoBehaviour
+{
+
+    public float speed = 3f;
+
+    void Start()
+    {
+
+        
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, speed);
+    }
+
+
+
+
+
+    }
