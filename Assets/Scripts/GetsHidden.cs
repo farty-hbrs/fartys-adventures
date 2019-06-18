9f1bec8dc70add21478b41ce4d5878de8eb2f552
farty-hbrs <50452003+farty-hbrs@users.noreply.github.com>
2019-06-18 02:06:29 +0200

    Hide/Reveal Script finished

--- Assets/Scripts/GetsHidden.cs
+++ Assets/Scripts/GetsHidden.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GetsHidden : MonoBehaviour
 {
+    public SpriteRenderer HideObject;
+    public SpriteRenderer RevealObject;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        HideObject.enabled = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        RevealObject.enabled = true;
     }
 }
