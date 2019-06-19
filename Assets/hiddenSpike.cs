7ebf9a529b981515f51edd68472f1595617c2ada
farty-hbrs <9GY517e8%FF6z1Q71685k5&aV>
2019-06-19 23:19:47 +0200

    HiddenSpikeScript
    I added this hidden spike script but unfurtanly i could not figur how i can make the player die

---
+++ Assets/hiddenSpike.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class hiddenSpike : MonoBehaviour
+{
+    private LevelManager levelmanger;
+    public float speed;
+    public float distance;
+
+    private Vector3 startPos;
+    private Vector3 newPos;
+    private Vector3 lastPos;
+    private SpriteRenderer spriteRenderer;
+
+    private GameObject target = null;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelmanger = FindObjectOfType<LevelManager>();
+        target = null;
+        startPos = transform.position;
+        lastPos = startPos;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GenerateNewPosition();
+        transform.position = newPos;
+    }
+     
+    void GenerateNewPosition()
+    {
+        newPos = startPos;
+        newPos.y += Mathf.PingPong(Time.time * speed, distance) - distance / 2;
+    }
+     
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            levelmanger.RespawnPlayer();
+        }
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            levelmanger.RespawnPlayer();
+        }
+    }
+
+}
