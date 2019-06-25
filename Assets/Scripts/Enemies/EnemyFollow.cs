181d1335fbe71486d1db54518fb7e42f92d6b3f5
Marco Marx <acc-git@marx.bz>
2019-06-25 21:10:33 +0200

    change PigAnimated

--- Assets/Scripts/Enemies/EnemyFollow.cs
+++ Assets/Scripts/Enemies/EnemyFollow.cs
@@ -7,43 +7,59 @@ public class EnemyFollow : MonoBehaviour
     public float speed = 4;
     public float minDist = 10;
     public float maxDist = 1;
+    public Collider2D killCollider;
 
-    private Transform player;
+    public float hitsToKill = 1;
+    
+    private GameObject player;
     private bool facingLeft;
+    private bool hittable;
+    private LevelManager levelManager;
+    private Rigidbody2D rbPlayer;
 
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        player = GameObject.FindWithTag("Player");
+        rbPlayer = player.GetComponent<Rigidbody2D>();
+        levelManager = FindObjectOfType<LevelManager>();
         facingLeft = true;
+        hittable = true;
     }
 
     void Update()
     {
         //transform.LookAt(player.transform);
 
-        if (transform.position.x > player.position.x && !facingLeft)
+        if (transform.position.x > player.transform.position.x && !facingLeft)
         {
             Flip();
         }
-        if (transform.position.x < player.position.x && facingLeft)
+        if (transform.position.x < player.transform.position.x && facingLeft)
         {
             Flip();
         }
 
-        if (Mathf.Abs(transform.position.x - player.position.x) >= minDist)
+        if (Mathf.Abs(transform.position.x - player.transform.position.x) >= minDist)
         {
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.position.x, transform.position.y), speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, transform.position.y), speed * Time.deltaTime);
             //transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 
 
 
-            if (Mathf.Abs(transform.position.x - player.position.x) <= maxDist)
+            if (Mathf.Abs(transform.position.x - player.transform.position.x) <= maxDist)
             {
                 // If the enemy can shoot or something like that you can implement it here
             }
 
         }
+    }
+
+    IEnumerator FreezePlayer()
+    {
+        yield return new WaitForSeconds(2f);
+        hittable = true;
+        killCollider.enabled = false;
     }
 
     void Flip()
